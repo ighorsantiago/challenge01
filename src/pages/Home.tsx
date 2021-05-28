25cb969ed1f4159eebfc718df936e94aa90b903f
Ighor Santiago <ighor_santiago@hotmail.com>
2021-05-28 10:16:03 -0300

    Resolução do desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
@@ -11,18 +12,35 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    
+    if(!newTaskTitle) {
+      Alert.alert('Não dá adicionar uma tarefa em branco...');
+      return;
+    }
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const task = tasks.filter((item) => item.id ===id)[0]
+    task.done = !task.done;
+    const newTasks = [task, ...tasks.filter(item => item.id !== task.id)];
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      tasks => tasks.id !== id
+    ));
   }
 
   return (
